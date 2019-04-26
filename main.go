be282b14ffd602f30df7b801ca406ccebd7fa0f9
Ramon Moraes <ramon.moraes94@gmail.com>
2019-04-26 08:18:23 -0300

    main using new module

--- main.go
+++ main.go
@@ -1,86 +1,14 @@
 package main
 
 import (
-	"errors"
 	"flag"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"regexp"
-	"strings"
+
+	gookdown "./cmd"
 )
 
 func main() {
 	summaryPath := flag.String("input", "summary.md", "File that contains the file paths to be compiled")
 	outputPath := flag.String("output", "README.md", "File path to the output file")
 	flag.Parse()
-
-	summary, err := ioutil.ReadFile(*summaryPath)
-	if err != nil {
-		fmt.Println("Could not find summary at", summaryPath)
-		log.Fatal(err)
-	}
-
-	lines := getLinesFromString(string(summary))
-	var paths []string
-	for _, line := range lines {
-		path, err := getPathFromReference(line)
-		if err != nil {
-			fmt.Println("Could not find path for", line)
-		}
-		if path != "" {
-			paths = append(paths, path)
-		}
-	}
-
-	compilePaths(*outputPath, paths)
-}
-
-func getLinesFromString(input string) []string {
-	lines := strings.Split(input, "\n")
-	var trimmedLines []string
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		if line == "" {
-			continue
-		}
-		trimmedLines = append(trimmedLines, line)
-	}
-	return trimmedLines
-}
-
-func getPathFromReference(line string) (string, error) {
-	re, _ := regexp.Compile(".*\\[(.*)\\]\\((.*)\\).*")
-	filePathGroup := 2
-
-	find := re.Find([]byte(line))
-	if len(find) < filePathGroup {
-		return "", errors.New("Could not find submatch")
-	}
-
-	submatchs := re.FindSubmatch([]byte(line))
-	return string(submatchs[filePathGroup]), nil
-}
-
-func compilePaths(outputPath string, filePaths []string) {
-	var content []byte
-	contentSeparator := []byte("\n\n")
-
-	for i, path := range filePaths {
-		data, err := ioutil.ReadFile(path)
-		if err != nil {
-			log.Fatal("File in path not fount", path)
-		}
-
-		if i != 0 {
-			content = append(content, contentSeparator...)
-		}
-
-		content = append(content, data...)
-	}
-
-	err := ioutil.WriteFile(outputPath, content, 0644)
-	if err != nil {
-		log.Fatal("Could not write file at", outputPath)
-	}
+	gookdown.Compile(*summaryPath, *outputPath)
 }
